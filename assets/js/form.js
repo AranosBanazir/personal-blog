1c432c79e101d17b55b94d95452dee45e2542d9d
Caleb Saiia <calebsaiia@gmail.com>
2024-03-14 17:45:40 -0500

    Added JS

---
+++ assets/js/form.js
@@ -0,0 +1,27 @@
+
+let defaultTheme = "day"
+let theme = defaultTheme
+const day_night =  document.getElementById(`day-night-pic`)
+const moonPath = "./assets/images/moon.png"
+const sunPath  = "./assets/images/sun.webp"
+const colorDarkMode = "dimgrey"
+const colorLightMode = "white"
+let background = document.getElementsByClassName("background")
+
+function themeSwitch(){
+    if (theme === "day"){
+        theme = "night"
+        day_night.src = moonPath
+
+        // iterates over elements with the 'background' class and applys the darkmode
+        for (const element of background){
+            element.style.backgroundColor = colorDarkMode
+        }
+    } else{
+        theme = "day"
+        day_night.src = sunPath
+        for (const element of background){
+            element.style.backgroundColor = colorLightMode
+        }
+    }
+}
