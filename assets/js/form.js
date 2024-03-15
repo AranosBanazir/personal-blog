2cda626e99c8fb656543d9456b0b2a44e0cca465
Caleb Saiia <115073504+AranosBanazir@users.noreply.github.com>
2024-03-15 01:09:22 -0500

    Added localStorage for blog posts

--- assets/js/form.js
+++ assets/js/form.js
@@ -9,7 +9,16 @@ const colorLightMode = "white"
 const background = document.getElementsByClassName("background")
 const labels = document.getElementsByTagName( "label" )
 let themeState = theme || "day"
+const blogPosts = []
+const darkStyle = {
+    color: "white",
+    backgroundColor: colorDarkMode,
+}
 
+const lightStyle = {
+    color: "black",
+    backgroundColor: "white",
+}
 
 
 //setting starting message for tooltip based on saved theme state
@@ -23,7 +32,10 @@ function themeSwitch(){
 
         // iterates over elements with the 'background' class and applys the darkmode
         for (const element of background){
-            element.style.backgroundColor = colorDarkMode
+            // element.style.backgroundColor = colorDarkMode
+            for (const key in darkStyle){
+                element.style[key] = darkStyle[key]
+            }
         }
 
         for (const element of labels){
@@ -46,4 +58,22 @@ function themeSwitch(){
         document.getElementById("day-night-tooltip").innerHTML = "Click to switch to night mode!"
 
     }
+}
+
+
+function newBlogPost(){
+    const username = document.getElementById("username-input").value
+    const title = document.getElementById("title-input").value;
+    const content = document.getElementById("content-box").value
+    var posts = JSON.parse(localStorage.getItem('posts') || '[]')
+    let newPost = {
+        username,
+        title,
+        content,
+    }   
+    posts.push(newPost)
+    localStorage.setItem( 'posts', JSON.stringify(posts)) 
+    
+    console.log(posts);
+    // document.getElementById("blog-form").reset()
 }
